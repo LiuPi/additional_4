660aa7c134592f9e2143563f7543e60e489a5fe4
Liudmila Pi <mizuxira@yandex.ru>
2017-09-15 18:08:21 +0300

    product/feature: implement product of two numbers function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function multiply(first, second) {
-  // your solution
+function multiplication(first, second) {
+    let x = first.split("").reverse();
+    let y = second.split("").reverse();
+    let result = [];
+
+    for (let i = 0; i < x.length; i++) {
+        for (let j = 0; j < y.length; j++) {
+            let index = i + j;
+            result[index] = x[i] * y[j] + (index >= result.length ? 0 : result[index]);
+            if (result[index] > 9) {
+                result[index + 1] = Math.floor(result[index] / 10) + (index + 1 >= result.length ? 0 : result[index + 1]);
+                result[index] -= Math.floor(result[index] / 10) * 10;
+            }
+        }
+    }
+    return result.reverse().join("");
 }
+
+module.exports = function multiply(first, second) {
+    return multiplication(first, second);
+};
